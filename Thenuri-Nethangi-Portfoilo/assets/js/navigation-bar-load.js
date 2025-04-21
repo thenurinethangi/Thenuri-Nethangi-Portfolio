75e57c8e58529af879b9d945c4dff9239a179f69
thenurinethangi <thenurinathangi@gmail.com>
2025-04-21 13:15:49 +0530

    updated code

--- Thenuri-Nethangi-Portfoilo/assets/js/navigation-bar-load.js
+++ Thenuri-Nethangi-Portfoilo/assets/js/navigation-bar-load.js
@@ -1,12 +1,14 @@
 let menu = document.getElementById("menu");
 console.log(menu);
 
-let pages = ['Home','About Me','Skill','Project','Contact'];
-let links = ['index.html','about-page.html','skill-page.html','project-page.html','contact-page.html'];
+let pages = ['Home', 'About Me', 'Skill', 'Project', 'Contact'];
+let links = ['index.html', 'about-page.html', 'skill-page.html', 'project-page.html', 'contact-page.html'];
 
-menu.addEventListener('click',loadNavigationBar);
+menu.addEventListener('click', loadNavigationBar);
 
-function loadNavigationBar(){
+function loadNavigationBar() {
+
+    let anchors = [];
 
     let bar = document.createElement('div');
     bar.style.width = '0%';
@@ -14,13 +16,14 @@ function loadNavigationBar(){
     bar.style.backgroundColor = 'rgba(20, 17, 26)';
     bar.style.zIndex = '100000';
     bar.style.position = 'absolute';
-    bar.style.borderBottomRightRadius = '1000%';
+    bar.style.right = '0';
+    bar.style.top = '0';
+    bar.style.borderBottomLeftRadius = '1000%';
     bar.style.display = 'flex';
     bar.style.flexDirection = 'column';
     bar.style.justifyContent = 'center';
     bar.style.alignItems = 'center';
     bar.style.gap = '3.5em';
-
 
     let close = document.createElement('img');
     close.src = 'assets/image/close-button.png';
@@ -31,48 +34,140 @@ function loadNavigationBar(){
     close.style.right = '0';
     close.style.margin = '2.5em 2em 0em 0em';
 
-    close.addEventListener('click',hey);
+    close.addEventListener('click', closeNavBar);
 
-    function hey(){
+    function closeNavBar() {
+        bar.animate(
+            [
+                { width: "100%", height: '100vh', borderBottomLeftRadius: '0%', offset: 0 },
+                { width: "0%", height: '0vh', borderBottomLeftRadius: '1000%', offset: 1 },
+            ],
+            {
+                duration: 800,
+                iterations: 1,
+                easing: "ease-in-out",
+                fill: 'forwards'
+            }
+        );
 
-        console.log("heyy");
+        close.animate(
+            [
+                { opacity: '1', offset: 0 },
+                { opacity: '0', offset: 1 }
+            ],
+            {
+                duration: 800,
+                iterations: 1,
+                easing: "ease-in-out",
+                fill: 'both'
+            }
+        );
+
+        anchors.forEach(anchor => {
+            anchor.animate(
+                [
+                    { opacity: '1', offset: 0 },
+                    { opacity: '0', offset: 1 }
+                ],
+                {
+                    duration: 500,
+                    easing: "ease-in-out",
+                    fill: 'both'
+                }
+            );
+        });
+
+        setTimeout(() => bar.remove(), 800);
     }
 
     bar.append(close);
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < pages.length; i++) {
         let a = document.createElement("a");
         a.innerHTML = pages[i];
-        a.style.width = '100%';
         a.style.textDecoration = 'none';
         a.style.fontSize = '1.15rem';
         a.style.textAlign = 'center';
         a.style.color = '#FFFFFF';
+        a.style.fontWeight = '300';
         a.href = links[i];
+        anchors.push(a);
+
+        a.addEventListener('mouseenter', () => {
+            a.style.background = 'linear-gradient(to right, #FFFFFF, #9747FF)';
+            a.style.webkitBackgroundClip = 'text';
+            a.style.webkitTextFillColor = 'transparent';
+        });
+
+        a.addEventListener('mouseleave', () => {
+            a.style.background = 'rgba(20, 17, 26)';
+            a.style.webkitTextFillColor = '#FFFFFF';
+            a.style.color = '#FFFFFF';
+        });
+
+        a.addEventListener('click', closeNavBarTwo);
+
+        function closeNavBarTwo() {
+            bar.animate(
+                [
+                    { width: "100%", height: '100vh', borderBottomLeftRadius: '0%', offset: 0 },
+                    { width: "0%", height: '0vh', borderBottomLeftRadius: '1000%', offset: 1 },
+                ],
+                {
+                    duration: 100,
+                    iterations: 1,
+                    easing: "ease-in-out",
+                    fill: 'forwards'
+                }
+            );
+
+            anchors.forEach(anchor => {
+                anchor.animate(
+                    [
+                        { opacity: '1', offset: 0 },
+                        { opacity: '0', offset: 1 }
+                    ],
+                    {
+                        duration: 100,
+                        easing: "ease-in-out",
+                        fill: 'both'
+                    }
+                );
+            });
+
+            setTimeout(() => bar.remove(), 100);
+        }
 
         bar.append(a);
     }
-    
-    
-    
-    
-    
-    
+
 
     bar.animate(
         [
-            { width: "0%", height: '0vh', borderBottomRightRadius: '1000%',  offset: 0 },
-            { width: "100%", height: '100vh', borderBottomRightRadius: '0%', offset: 1 },
+            { width: "0%", height: '0vh', borderBottomLeftRadius: '1000%', offset: 0 },
+            { width: "100%", height: '100vh', borderBottomLeftRadius: '0%', offset: 1 },
         ],
         {
             duration: 800,
             iterations: 1,
-            easing: "linear",
+            easing: "ease-in-out",
             fill: 'forwards'
         }
     );
 
-    let body = document.getElementsByTagName('body');
-    body[0].prepend(bar);
+    anchors.forEach(anchor => {
+        anchor.animate(
+            [
+                { opacity: '0', offset: 0 },
+                { opacity: '1', offset: 1 }
+            ],
+            {
+                duration: 800,
+                easing: "ease-in-out",
+                fill: 'both'
+            }
+        );
+    });
 
+    document.body.prepend(bar);
 }
